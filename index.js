7894d29282eebef3b660c27c54242cd9e510969a
naveen <naveenmalineni7@gmail.com>
2021-09-16 17:12:45 +0530

    v 1

--- index.js
+++ index.js
@@ -10,30 +10,48 @@ function setCity(val){
 
 let contentDivEl = document.querySelector('.final-details');
 
-function displayData(data) {
-    const {dt,description,humidity_in_percent,pressure_in_hPa,temp_in_celsius} = data;
-    const arr = [city,description,temp_in_celsius,pressure_in_hPa,dt]
-    count = count + 1
-    const ulEl = document.createElement('ul');
-    contentDivEl.appendChild(ulEl);
-    ulEl.classList.add("content")
-    arr.map((item,index) => {
-        let liEl = document.createElement('li');
-        if(index === 1) {
-            const ipEl = document.createElement('input');
-            ipEl.value = item;
-            liEl.appendChild(ipEl);
-            ipEl.classList.add("content-input");
-        }
-        else{
-            liEl.textContent = item;
-        }
-        ulEl.appendChild(liEl);
-        liEl.classList.add("city-content-data");
+
+
+
+function renderData(){
+    contentDivEl.innerHTML = ''
+    searchedData.map( cityData => {
+        const {city,dt,description,humidity_in_percent,pressure_in_hPa,temp_in_celsius,uid} = cityData;
+        const arr = [city,description,temp_in_celsius,pressure_in_hPa,dt];
+        const ulEl = document.createElement('ul');
+        contentDivEl.appendChild(ulEl);
+        ulEl.classList.add("content")
+        arr.map((item,index) => {
+            let liEl = document.createElement('li');
+            if(index === 1) {
+                const ipEl = document.createElement('input');
+                ipEl.value = item;
+                liEl.appendChild(ipEl);
+                ipEl.classList.add("content-input");
+            }
+            else{
+                liEl.textContent = item;
+            }
+            ulEl.appendChild(liEl);
+            liEl.classList.add("city-content-data");
+        })
+        const delEl = document.createElement("li");
+        delEl.textContent ="delete";
+        ulEl.appendChild(delEl);
+        delEl.classList.add("del-item");
+        delEl.style.cursor = "pointer";
     })
+}
+
+
+
+
+function displayData(data) {
+    count = count + 1
     data.uid = count;
+    data.city = city;
     searchedData.push(data);
-    console.log(searchedData);
+    renderData();
 }
 
 const fetchData = async () => {
